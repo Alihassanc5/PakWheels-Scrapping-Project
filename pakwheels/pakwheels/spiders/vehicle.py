9a3d562b0f5d49727ae887292e3b68b6913b9211
Alihassanc5 <Alihassan01264@gmail.com>
2022-08-16 18:42:29 +0500

    I fixed naming issues.

--- pakwheels/pakwheels/spiders/vehicle.py
+++ pakwheels/pakwheels/spiders/vehicle.py
@@ -3,7 +3,7 @@ from scrapy.spiders import CrawlSpider, Rule
 
 
 class VehicleSpider(CrawlSpider):
-    name = 'Bikes'
+    name = 'Vehicle'
     allowed_domains = ['www.pakwheels.com']
     used_bikes_url = 'https://www.pakwheels.com/used-bikes/search/-/'
     
@@ -12,19 +12,19 @@ class VehicleSpider(CrawlSpider):
     ]
     
     rules = (
-        Rule(LinkExtractor(restrict_css='.search-title a'), callback='parse_item', follow=False),
+        Rule(LinkExtractor(restrict_css='.search-title a'), callback='parse_vehicle', follow=False),
         Rule(LinkExtractor(restrict_css='.next_page a'), follow=True)
     )
 
-    def parse_item(self, response):
+    def parse_vehicle(self, response):
         url = response.url
         price = response.css('.price-box strong::text').get()
         title = response.css('.well h1::text').get()
         location = response.css('.well p a::text').get()
         engine_data = response.css('.table.table-bordered.text-center.table-engine-detail.fs16  p::text').getall()
         registered_in = response.css('.table.table-featured.nomargin td::text').getall()[1]
-        body_type = response.css('.table.table-featured.nomargin td a::text').get()         
-        engine_year, engine_millage, engine_type = engine_data 
+        body_type = response.css('.table.table-featured.nomargin td a::text').get()
+        engine_year, engine_millage, engine_type = engine_data
         
         yield {
             'Url': url,
