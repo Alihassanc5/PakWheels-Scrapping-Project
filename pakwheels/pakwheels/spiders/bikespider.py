91989f02f803634708cb08a614b057baba94933a
Alihassanc5 <Alihassan01264@gmail.com>
2022-08-17 12:47:14 +0500

    I have fixed some css issues for extraction

--- pakwheels/pakwheels/spiders/bikespider.py
+++ pakwheels/pakwheels/spiders/bikespider.py
@@ -5,7 +5,7 @@ from scrapy.spiders import CrawlSpider, Rule
 class BikeSpider(CrawlSpider):
     name = 'Bike'
     allowed_domains = ['www.pakwheels.com']
-    start_urls = ['https://www.pakwheels.com/used-bikes/honda-cd-70-lahore/56648']
+    start_urls = ['https://www.pakwheels.com/used-bikes/search/-/']
     
     rules = (
         Rule(
@@ -21,10 +21,10 @@ class BikeSpider(CrawlSpider):
         price = response.css('.price-box strong::text').get()
         title = response.css('.well h1::text').get()
         location = response.css('.well p a::text').get()
-        engine_year = response.css('.table.table-bordered.text-center.table-engine-detail.fs16  p::text').get()
-        engine_millage = response.css('.table.table-bordered.text-center.table-engine-detail.fs16  p::text')[1].get()
-        engine_type = response.css('.table.table-bordered.text-center.table-engine-detail.fs16  p::text')[2].get()
-        registered_in = response.css('.table.table-featured.nomargin td::text')[1].get()
+        engine_year = response.css('.engine-icon.year + p::text').get()
+        engine_millage = response.css('.engine-icon.millage + p::text').get()
+        engine_type = response.css('.engine-icon.type + p::text').get()
+        registered_in = response.css('.table.table-featured.nomargin td:first-child + td::text').get()
         body_type = response.css('.table.table-featured.nomargin td a::text').get()
         
         yield {
